258d07b50316385df64416833a05e7da59c743d9
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-19 11:03:07 -0700

    add email field to client model

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -5,6 +5,7 @@ namespace HairSalon.Models
     public int ClientId { get; set; }
     public string Name { get; set; }
     public string Phone { get; set; }
+    public string Email { get; set; }
     public int StylistId { get; set; }
     public Stylist stylist { get; set; }
   }
